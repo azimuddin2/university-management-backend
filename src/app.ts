30ed834c17efeb0fda8abceae984edd7e84f84c7
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-11-22 22:09:15 +0600

    development package add

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app: Application = express()
+const app: Application = express();
 
 // parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Developer')
-})
+  res.send('Hello Developer');
+});
 
-export default app
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './app/config'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    await mongoose.connect(config.database_url as string);
+    console.log('Connection to database is successfully established');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-main()
+main();
