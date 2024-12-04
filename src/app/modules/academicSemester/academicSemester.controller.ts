7ac8a2b0921da68498cd121e4d2d6177e82b5722
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-12-04 12:56:02 +0600

    academic semester route, controller andservice

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,20 @@
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicSemesterServices } from './academicSemester.service';
+
+const createAcademicSemester = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body
+  );
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Academic semester created successfully',
+    data: result,
+  });
+});
+
+export const AcademicSemesterControllers = {
+  createAcademicSemester,
+};
