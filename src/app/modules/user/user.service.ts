3dc1b3423198c3fc11371b5005bb52896eafe267
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-12-11 22:57:51 +0600

    userService using transaction & rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TStudent } from '../student/student.interface';
@@ -5,6 +6,7 @@ import Student from '../student/student.model';
 import { TUser } from './user.interface';
 import User from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppError';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   const userData: Partial<TUser> = {};
@@ -20,21 +22,37 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester
   );
 
-  // set generated id
-  userData.id = await generateStudentId(admissionSemester);
+  const session = await mongoose.startSession();
 
-  const newUser = await User.create(userData);
+  try {
+    session.startTransaction();
+    // set generated id
+    userData.id = await generateStudentId(admissionSemester);
 
-  if (Object.keys(newUser).length) {
-    payload.id = newUser.id;
-    payload.user = newUser._id;
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session });
+    if (!newUser.length) {
+      throw new AppError(400, 'Failed to create user');
+    }
 
-    const newStudent = await Student.create(payload);
+    // set id, _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id;
+
+    // create a student (transaction-2)
+    const newStudent = await Student.create([payload], { session });
+    if (!newStudent.length) {
+      throw new AppError(400, 'Failed to create student');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
 
     return newStudent;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
   }
-
-  return newUser;
 };
 
 export const UserServices = {
