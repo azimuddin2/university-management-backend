8db60a22686a786ffa4bb13072b8445f8afd809d
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-15 13:05:43 +0600

    create enrolled course business logic

--- src/app/modules/EnrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -1,9 +1,86 @@
+import mongoose from 'mongoose';
+import AppError from '../../errors/AppError';
+import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
+import Student from '../student/student.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
+import { EnrolledCourse } from './enrolledCourse.model';
 
 const createEnrolledCourseIntoDB = async (
   userId: string,
   payload: TEnrolledCourse
-) => {};
+) => {
+  const { offeredCourse } = payload;
+
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(404, 'Offered Course not found!');
+  }
+
+  if (isOfferedCourseExists.maxCapacity <= 0) {
+    throw new AppError(400, 'Room is full!');
+  }
+
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
+
+  if (!student) {
+    throw new AppError(404, 'Student not found!');
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  });
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(409, 'Student is already enrolled!');
+  }
+
+  const enrolledCourseData = {
+    semesterRegistration: isOfferedCourseExists.semesterRegistration,
+    academicSemester: isOfferedCourseExists.academicSemester,
+    academicFaculty: isOfferedCourseExists.academicFaculty,
+    academicDepartment: isOfferedCourseExists.academicDepartment,
+    offeredCourse: offeredCourse,
+    course: isOfferedCourseExists.course,
+    student: student._id,
+    faculty: isOfferedCourseExists.faculty,
+    isEnrolled: true,
+  };
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const result = await EnrolledCourse.create([enrolledCourseData], {
+      session,
+    });
+
+    if (!result) {
+      throw new AppError(400, 'Failed to enroll in this course!');
+    }
+
+    const maxCapacity = isOfferedCourseExists.maxCapacity;
+    await OfferedCourse.findByIdAndUpdate(
+      offeredCourse,
+      {
+        maxCapacity: maxCapacity - 1,
+      },
+      { new: true }
+    );
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(error);
+  }
+};
 
 export const EnrolledCourseServices = {
   createEnrolledCourseIntoDB,
